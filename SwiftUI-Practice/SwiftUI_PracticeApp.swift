a93b8cfcb82cce715c3c86cc793f305068785df1
Artun Erol <artun.erol@yahoo.com>
2023-12-18 11:57:32 +0300

    Initial Commit

---
+++ SwiftUI-Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI-Practice
+//
+//  Created by Artun Erol on 18.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
